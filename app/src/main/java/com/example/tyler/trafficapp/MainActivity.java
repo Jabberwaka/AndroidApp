aa72cde685af8bfc5fe8749f1584e440526ff6af
Sidhant Dixit <mishrasidhant@gmail.com>
2018-10-31 02:34:27 -0400

    1. Comments added on top of some main functions. 2. Commented out certain functions to be implemented at a later date (when adding multitasking, changing focus, minimizing app, etc) 3. Added String "msg" to MainActivity to be used via Log.d to print logging info to "Logcat" (can be used when testing) 4. TODO: Update strings.xml to remove hard coded strings in various activites

--- app/src/main/java/com/example/tyler/trafficapp/MainActivity.java
+++ app/src/main/java/com/example/tyler/trafficapp/MainActivity.java
@@ -18,11 +18,15 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+    String msg = "Android : "; //Added to implement logging functionality
 
+    /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+
         Button button = findViewById(R.id.button);
         Button button2 = findViewById(R.id.button6);
 
@@ -59,6 +63,50 @@ public class MainActivity extends AppCompatActivity {
         TextView text = findViewById(R.id.textView2);
         text.setText(SQLConnect()); //to prove sql server connected
     }
+    /** Called when the activity is about to become visible. */
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+    */
+
+    /** Called when the activity has become visible. */
+    /*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+    */
+
+    /** Called when another activity is taking focus. */
+    /*
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+    */
+
+    /** Called when the activity is no longer visible. */
+    /*
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+    */
+
+    /** Called just before the activity is destroyed. */
+    /*
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+    */
 
     public void viewCamera(View view){
         Intent viewTraffic = new Intent(this, DisplayCameraActivity.class);
